ab7f0049f2fb95acd8b420bdd5c86f310b839a6b
Ryan Duran <dogoathena@gmail.com>
2024-02-20 10:42:17 -0800

    1st

--- 1-vector.js
+++ 1-vector.js
@@ -1,5 +1,27 @@
 // Write a class Vec that represents a vector in two-dimensional space. It takes x and y parameters (numbers), which it should save to properties of the same name.
 
+    class Vec {
+        constructor(x, y) {
+            this.x = x
+            this.y = y
+        }
+        plus(add) {
+            this.x = this.x + add.x
+            this.y = this.y + add.y
+            return(this)
+        }
+        minus(sub) {
+            this.x = this.x - sub.x
+            this.y = this.y - sub.y
+            return(this)
+        }
+        get length() {
+            let x = this.x
+            let y = this.y
+            return(Math.sqrt((x * x) + (y * y)))
+        }
+    }
+
 // Give the Vec prototype two methods, plus and minus, that take another vector as a parameter and return a new vector that has the sum or difference of the two vectors’ (this and the parameter) x and y values.
 
 // Add a getter property length to the prototype that computes the length of the vector—that is, the distance of the point (x, y) from the origin (0, 0).
